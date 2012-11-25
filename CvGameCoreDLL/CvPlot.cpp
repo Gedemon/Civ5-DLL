218443917643cb8c5a4aba0aadec00d11900c0be
Gedemon <gedemon@free.fr>
2012-11-25 22:54:41 +0100

    Remove conflicting check with RED_Base (Air units in friendly city belongs to Base branch, not SpecialStack...)

--- CvGameCoreDLL/CvPlot.cpp
+++ CvGameCoreDLL/CvPlot.cpp
@@ -3923,10 +3923,7 @@ int CvPlot::getNumFriendlyUnitsOfType(const CvUnit * pUnit, bool bBreakOnUnitLim
 			if (!kUnitTeam.isAtWar(pLoopUnit->getTeam()))
 			{
 				// Units of the same type OR Units belonging to different civs
-				//if (pUnit->getOwner() != pLoopUnit->getOwner() || pLoopUnit->AreUnitsOfSameType(*pUnit, bPretendEmbarked))
-				// RED : for air unit in foreign cities, we must allows units belonging to different civs
-				if ((pUnit->getOwner() != pLoopUnit->getOwner() && pUnit->getDomainType() != DOMAIN_AIR ) || pLoopUnit->AreUnitsOfSameType(*pUnit, bPretendEmbarked) || (pLoopUnit->AreUnitsOfSameType(*pUnit, bPretendEmbarked) && !GC.getGame().isOption("GAMEOPTION_REBASE_IN_FRIENDLY_CITY")))
-				// RED
+				if (pUnit->getOwner() != pLoopUnit->getOwner() || pLoopUnit->AreUnitsOfSameType(*pUnit, bPretendEmbarked))
 				{
 					// We should allow as many cargo units as we want
 					if (!pLoopUnit->isCargo())
