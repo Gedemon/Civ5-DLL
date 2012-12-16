afad55004bdbe94da0e30b94c351116fe7e09491
Gedemon <gedemon@free.fr>
2012-12-16 17:36:49 +0100

    Fix "GAMEOPTION_CAN_ENTER_FOREIGN_CITY", and restrict enter city authorization for minor civilizations (must have allied level)

--- CvGameCoreDLL/CvUnit.cpp
+++ CvGameCoreDLL/CvUnit.cpp
@@ -1904,7 +1904,7 @@ bool CvUnit::canEnterTerritory(TeamTypes eTeam, bool bIgnoreRightOfPassage, bool
 				return true;
 			}
 
-			if (bIsCity && bIsDeclareWarMove)
+			if (bIsCity && bIsDeclareWarMove && !GC.getGame().isOption("GAMEOPTION_CAN_ENTER_FOREIGN_CITY"))
 			{
 				return false;
 			}
@@ -2276,6 +2276,13 @@ bool CvUnit::canMoveInto(const CvPlot & plot, byte bMoveFlags) const
 		{
 			if (plot.isCity() && plot.getPlotCity()->getOwner() != getOwner())
 				// RED <<<<<
+				if (GET_PLAYER(plot.getPlotCity()->getOwner()).isMinorCiv()) // special check for minor civs: we don't want to allow basing of units in cities unless we have allied level...
+				{
+					CvMinorCivAI* pMinorAI = GET_PLAYER(plot.getPlotCity()->getOwner()).GetMinorCivAI();
+					if (pMinorAI->GetFriendshipWithMajor(getOwner()) < pMinorAI->GetAlliesThreshold())
+						return false;
+				}
+
 				if (getDomainType() != DOMAIN_AIR && !GC.getGame().isOption("GAMEOPTION_CAN_ENTER_FOREIGN_CITY"))
 				// RED >>>>>
 				return false;
