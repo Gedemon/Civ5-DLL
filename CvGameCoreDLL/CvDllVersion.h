f7a8cb676b28ec5ef852146d9474ca8a37094504
Gedemon <gedemon@free.fr>
2013-07-20 12:36:51 +0200

    Set DLL GUID

--- CvGameCoreDLL/CvDllVersion.h
+++ CvGameCoreDLL/CvDllVersion.h
@@ -12,9 +12,9 @@
 // WARNING! This must be unique for each DLL!
 // If you are a modder and you are creating your own DLL from the original source, this must be changed to a unique GUID.
 // Use the Visual Studio Create GUID option in the Tools menu to create a new GUID.
-// {478F7A6E-9126-4a1c-8ECD-7B971E99A2B1}
+// {48CD1991-DA52-40f9-A90E-6D52C3D06C5E}
 static const GUID CIV5_CORE_RELEASE_DLL_GUID =
-{ 0x478f7a6e, 0x9126, 0x4a1c, { 0x8e, 0xcd, 0x7b, 0x97, 0x1e, 0x99, 0xa2, 0xb1 } };
+{ 0x48cd1991, 0xda52, 0x40f9, { 0xa9, 0xe, 0x6d, 0x52, 0xc3, 0xd0, 0x6c, 0x5e } };
 
 static const char *CIV5_CORE_RELEASE_DLL_VERSION = "1.0.0";
 //------------------------------------------------------------------------------
