5e2aaefc1c5ca29fc778529fa9785065c8bfb731
Gedemon <gedemon@free.fr>
2013-07-30 23:48:03 +0200

    Add "GAMEOPTION_BEST_DEFENDER_BY_HEALTH", when checked the combat functions take the unit that will have the most Health left after the attack to defend a plot. Add CvUnit::isMarkedBestDefender() and CvUnit::setMarkedBestDefender(bool bNewValue) to temporary force an unit to defend a plot (exposed to Lua)

--- CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
@@ -376,6 +376,10 @@ void CvLuaUnit::PushMethods(lua_State* L, int t)
 	Method(GetExperiencePercent);
 	Method(GetKamikazePercent);
 
+	// RED <<<<<<
+	Method(IsMarkedBestDefender);
+	Method(SetMarkedBestDefender);
+	// RED >>>>>>
 
 	Method(IsOutOfAttacks);
 	Method(SetMadeAttack);
@@ -3707,6 +3711,28 @@ int CvLuaUnit::lGetKamikazePercent(lua_State* L)
 	lua_pushinteger(L, iResult);
 	return 1;
 }
+// RED <<<<<<
+//------------------------------------------------------------------------------
+//bool isMarkedBestDefender();
+int CvLuaUnit::lIsMarkedBestDefender(lua_State* L)
+{
+	CvUnit* pkUnit = GetInstance(L);
+	const bool bResult = pkUnit->isMarkedBestDefender();
+
+	lua_pushboolean(L, bResult);
+	return 1;
+}
+//------------------------------------------------------------------------------
+//void setMarkedBestDefender(bool bNewValue);
+int CvLuaUnit::lSetMarkedBestDefender(lua_State* L)
+{
+	CvUnit* pkUnit = GetInstance(L);
+	const bool bNewValue = lua_toboolean(L, 2);
+
+	pkUnit->setMarkedBestDefender(bNewValue);
+	return 0;
+}
+// RED >>>>>
 //------------------------------------------------------------------------------
 //bool isOutOfAttacks();
 int CvLuaUnit::lIsOutOfAttacks(lua_State* L)
